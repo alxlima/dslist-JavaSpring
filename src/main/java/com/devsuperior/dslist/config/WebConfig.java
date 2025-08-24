847b721c494d55fd120a9939aaf488abac999b12
Alex Lima <alex_lima2013@hotmail.com>
2025-08-24 02:42:50 -0300

    CORS config ajustes

--- src/main/java/com/devsuperior/dslist/config/WebConfig.java
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -1,6 +1,7 @@
-//Classe que configuro o cors. origins. no aplication.properties. exemplo. url https://localhost:porta:7153 etc..
+// Classe que configuro o cors. origins. no aplication.properties. exemplo. url https://localhost:porta:7153 etc..
+// Difino quais endereços url(web,sites), estão autorizado a acessar este backend.
 // por padrão, se variavel de ambiente não existir na hospedagem. libero para o endereço local hosto. se não para endereço dominio. 
-// caso configurar adicionoar variavel de ambiente [ CORS_ORIGINS:https//: nome.dominios] - isso libera o meu navegador, apenas para este dominio poder acessar.
+// caso configurar adicionar variavel de ambiente [ CORS_ORIGINS:https//: nome.dominios] - isso libera o meu navegador, apenas para este dominio poder acessar.
 package com.devsuperior.dslist.config;
 
 import org.springframework.beans.factory.annotation.Value;
